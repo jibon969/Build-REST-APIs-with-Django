92add7bb93bc1784ce39cfe0e81bc833236bd713
= <=>
2021-12-01 15:52:34 +0600

    added new model Movie

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+    class Meta:
+        model = Movie
+
+
+admin.site.register(Movie, MovieAdmin)
+
+
