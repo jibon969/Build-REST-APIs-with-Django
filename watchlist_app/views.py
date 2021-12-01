5488a9279d9ff8c3aa6e0ffe804c2f226208ac23
= <=>
2021-12-01 16:36:30 +0600

    added movie_details

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -6,9 +6,19 @@ from django.http import JsonResponse
 def movie_list(request):
     movies = Movie.objects.all()
     # print(type(movies))
-    # print(movies.values())
+    # print(movies.values())m
     data = {
         'movies': list(movies.values())
     }
 
+    return JsonResponse(data)
+
+
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    data = {
+        'name': movie.name,
+        'description': movie.description,
+        'active': movie.active,
+    }
     return JsonResponse(data)
