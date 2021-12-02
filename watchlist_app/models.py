088fde6aadba963f55f6cc8ecfbedc66e58cb765
= <=>
2021-12-02 11:23:02 +0600

    added install django-rest framework

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -6,6 +6,5 @@ class Movie(models.Model):
     description = models.CharField(max_length=200)
     active = models.BooleanField(default=True)
 
-
     def __str__(self):
         return self.name
--- watchmate/settings.py
+++ watchmate/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'watchlist_app.apps.WatchlistAppConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
