67c806937ffdbd4e026778f41c20008e86d31b7c
= <=>
2021-12-02 16:05:48 +0600

    added status code api

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from watchlist_app.models import Movie
@@ -23,7 +24,10 @@ def movie_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail(request, pk):
     if request.method == "GET":
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'Errors': 'Movie Not Found'}, status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
 
@@ -33,10 +37,10 @@ def movie_detail(request, pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == "DELETE":
         movie = Movie.objects.get(pk=pk)
         movie.delete()
-        return Response()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
