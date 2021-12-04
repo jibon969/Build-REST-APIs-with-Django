198546abc099c92f75ab8c1ee0a533d2aaf8f724
= <=>
2021-12-04 13:22:33 +0600

    added get & post method watch_list

--- movie_stream_app/api/views.py
+++ movie_stream_app/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, APIView
 from movie_stream_app.models import StreamPlatform, WatchList
@@ -6,9 +7,19 @@ from movie_stream_app.api.serialization import StreamPlatformSerializer, WatchLi
 
 # Function based view
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def watch_list(request):
-    queryset = WatchList.objects.all()
-    serializer = WatchListSerializer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        queryset = WatchList.objects.all()
+        serializer = WatchListSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = WatchListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
