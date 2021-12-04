c0d657baa53c3356f9cacbfc7cebd506ead9ddd8
= <=>
2021-12-04 12:58:25 +0600

    added new model

--- movie_stream_app/admin.py
+++ movie_stream_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import StreamPlatform, WatchList
 
-# Register your models here.
+
+admin.site.register(StreamPlatform)
+admin.site.register(WatchList)
