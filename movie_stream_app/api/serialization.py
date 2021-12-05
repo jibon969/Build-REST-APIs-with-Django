46df94bee448fd2ff09a9d2dcdbc9dd9ddcece9a
= <=>
2021-12-05 14:28:16 +0600

    added Review models

--- movie_stream_app/api/serialization.py
+++ movie_stream_app/api/serialization.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
-from movie_stream_app.models import StreamPlatform, WatchList
+from movie_stream_app.models import WatchList, StreamPlatform
+
+
+class WatchListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = WatchList
+        fields = "__all__"
 
 
 class StreamPlatformSerializer(serializers.ModelSerializer):
+    watchlist = WatchListSerializer(many=True, read_only=True)
+
     class Meta:
         model = StreamPlatform
         fields = "__all__"
 
 
-class WatchListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WatchList
-        fields = "__all__"
